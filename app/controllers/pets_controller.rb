e9abd4478f28e42735710332073f610d466d4f22
Marlon Merjos <marlonmerjos@gmail.com>
2017-08-30 17:48:54 -0400

    Done.

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,25 +2,42 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
-  post '/pets' do 
-
+  post '/pets' do
+    @pet = Pet.new
+    @pet.name = params[:pet][:name]
+    @pet.owner = Owner.find_by(id: params[:pet][:owner])
+    if params[:pet][:owner].nil?
+      @pet.owner = Owner.find_or_create_by(name: params[:owner_name])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @pet = Pet.find_by(id: params[:id])
+    erb :"/pets/edit"
+  end
+
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
-
+  post '/pets/:id' do
+    @pet = Pet.find_by(id: params[:id])
+    @pet.update(params[:pet])
+    if params[:owner][:name].size > 0
+      @pet.owner = Owner.create(params[:owner])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
